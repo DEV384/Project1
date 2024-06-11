2bed238cf0765fbe8ab205850d397e75bd5b36d3
Dev384 <devprajapati384@gmail.com>
2024-06-11 16:42:55 +0530

    commit

--- db/connect.js
+++ db/connect.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
-uri = "mongodb+srv://Project1:dev189$7046@project1.yt4x1eh.mongodb.net/Project1?retryWrites=true&w=majority";
+uri = "mongodb+srv://Project1:dev189p7046@project1.yt4x1eh.mongodb.net/Project1?retryWrites=true&w=majority";
 
 
 const connectDB = ()=> {
+
     console.log("connect DB");
     return mongoose.connect(uri);
 };
